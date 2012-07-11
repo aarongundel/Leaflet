1619763ac1787322f72ba490673d899c8d23d3a6
Aaron Gundel <aaron.gundel@gmail.com>
2012-07-11 10:25:57 -0700

    added support for TMS style urls

--- src/layer/tile/TileLayer.GeoJSON.js
+++ src/layer/tile/TileLayer.GeoJSON.js
@@ -1,75 +1,147 @@
 L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
-	defaultParams: {
-		
+	options: {
+		debug: false
 	},
 
 	initialize: function (url, options) { // (String, Object)
 		this._url = url;
 
-		var params = L.Util.extend({}, this.defaultParams);
-
-		for (var i in options) {
-			// all keys that are not TileLayer options go to WMS params
-			if (!this.options.hasOwnProperty(i)) {
-				wmsParams[i] = options[i];
-			}
-		}
-
-		this.wmsParams = wmsParams;
-
 		L.Util.setOptions(this, options);
 	},
 
-	getXHR: function () {
+	_getXHR: function () {
 		if (window.XMLHttpRequest
-			&& ('file:' != window.location.protocol || !window.ActiveXObject)) {
-			return new XMLHttpRequest;
+			&& ('file:' !== window.location.protocol || !window.ActiveXObject)) {
+			return new XMLHttpRequest();
 		} else {
-			try { return new ActiveXObject('Microsoft.XMLHTTP'); } catch(e) {}
-			try { return new ActiveXObject('Msxml2.XMLHTTP.6.0'); } catch(e) {}
-			try { return new ActiveXObject('Msxml2.XMLHTTP.3.0'); } catch(e) {}
-			try { return new ActiveXObject('Msxml2.XMLHTTP'); } catch(e) {}
+			try { return new ActiveXObject('Microsoft.XMLHTTP'); } catch (e) {}
+			try { return new ActiveXObject('Msxml2.XMLHTTP.6.0'); } catch (e) {}
+			try { return new ActiveXObject('Msxml2.XMLHTTP.3.0'); } catch (e) {}
+			try { return new ActiveXObject('Msxml2.XMLHTTP'); } catch (e) {}
 		}
 		return false;
-	}
+	},
 
-	request: function (url, callback, mimeType) {
+	drawTile: function (canvas, tilePoint, zoom) {
+		var ctx = {
+			canvas: canvas,
+			tile: tilePoint,
+			zoom: zoom
+		};
+
+		if (this.params.debug) {
+			this._drawDebugInfo(ctx);
+		}
+		this._draw(ctx);
+	},
+
+	_request: function (url, callback, mimeType) {
 		var req;
 
 		function send () {
-			req = getXHR();
+			req = _getXHR();
 			if (mimeType && req.overrideMimeType) {
 				req.overrideMimeType(mimeType);
 			}
 			req.open("GET", url, true);
-			req.onreadystatechange = function(e) {
-			if (req.readyState == 4) {
-				active--;
-				if (req.status < 300) callback(req);
+			req.onreadystatechange = function (e) {
+				if (req.readyState === 4) {
+					active--;
+					if (req.status < 300) {
+						callback(req);
+					}
 					process();
 				}
 			};
 			req.send(null);
 		}
-		function abort(hard) {
-			if (dequeue(send)) return true;
-			if (hard && req) { req.abort(); return true; }
+		function abort (hard) {
+			if (dequeue(send)) {
+				return true;
+			}
+			if (hard && req) {
+				req.abort(); return true;
+			}
 			return false;
 		}
 
-		queued.push(send);
-		process();
-		return {abort: abort};
-	}
+		return {abort: abort,send: send};
+	},
+
+	_tilePoint: function (ctx, coords) {
+		// start coords to tile 'space'
+		var s = ctx.tile.multiplyBy(this.tileSize), p, x, y;
+
+		// actual coords to tile 'space'
+		p = this._map.project(new L.LatLng(coords[1], coords[0]));
+
+		// point to draw		
+		x = Math.round(p.x - s.x);
+		y = Math.round(p.y - s.y);
+		return {
+			x: x,
+			y: y
+		};
+	},
+
+	_clip: function (ctx, points) {
+		var nw = ctx.tile.multiplyBy(this.tileSize),
+			se = nw.add(new L.Point(this.tileSize, this.tileSize)),
+			bounds = new L.Bounds([nw, se]),
+			len = points.length,
+			out = [], i, seg;
+
+		for (i = 0; i < len - 1; i = i + 1) {
+			seg = L.LineUtil.clipSegment(points[i], points[i + 1], bounds, i);
+			if (!seg) {
+				continue;
+			}
+			out.push(seg[0]);
+			// if segment goes out of screen, or it's the last one, it's the end of the line part
+			if ((seg[1] !== points[i + 1]) || (i === len - 2)) {
+				out.push(seg[1]);
+			}
+		}
+		return out;
+	},
+
+	_isActuallyVisible: function (coords) {
+		var coord = coords[0],
+			min = [coord.x, coord.y],
+			max = [coord.x, coord.y],
+			diff0, diff1, visible, i;
+
+		for (i = 1; i < coords.length; i++) {
+			coord = coords[i];
+			min[0] = Math.min(min[0], coord.x);
+			min[1] = Math.min(min[1], coord.y);
+			max[0] = Math.max(max[0], coord.x);
+			max[1] = Math.max(max[1], coord.y);
+		}
+
+		diff0 = max[0] - min[0];
+		diff1 = max[1] - min[1];
+
+		if (this.options.debug) {
+			console.log(diff0 + ' ' + diff1);
+		}
+		
+		visible = diff0 > 1 || diff1 > 1;
+
+		return visible;
+	},
 
 	_drawPoint: function (ctx, geom, style) {
+		
+		var p, c, g;
+
 		if (!style) {
 			return;
 		}
 		
-		var p = this._tilePoint(ctx, geom);
-		var c = ctx.canvas;
-		var g = c.getContext('2d');
+		p = this._tilePoint(ctx, geom);
+		c = ctx.canvas;
+		g = c.getContext('2d');
 		g.beginPath();
 		g.fillStyle = style.color;
 		g.arc(p.x, p.y, style.radius, 0, Math.PI * 2);
@@ -83,9 +155,10 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 			return;
 		}
 		
-		var coords = geom, proj = [], i;
+		var coords = geom, proj = [], i, g;
 		coords = this._clip(ctx, coords);
 		coords = L.LineUtil.simplify(coords, 1);
+
 		for (i = 0; i < coords.length; i++) {
 			proj.push(this._tilePoint(ctx, coords[i]));
 		}
@@ -93,7 +166,7 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 			return;
 		}
 
-		var g = ctx.canvas.getContext('2d');
+		g = ctx.canvas.getContext('2d');
 		g.strokeStyle = style.color;
 		g.lineWidth = style.size;
 		g.beginPath();
@@ -105,13 +178,31 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 		g.restore();
 	},
 
+	_drawDebugInfo: function (ctx) {
+		var max = this.tileSize, 
+			g = ctx.canvas.getContext('2d');
+		g.strokeStyle = '#000000';
+		g.fillStyle = '#FFFF00';
+		g.strokeRect(0, 0, max, max);
+		g.font = "12px Arial";
+		g.fillRect(0, 0, 5, 5);
+		g.fillRect(0, max - 5, 5, 5);
+		g.fillRect(max - 5, 0, 5, 5);
+		g.fillRect(max - 5, max - 5, 5, 5);
+		g.fillRect(max / 2 - 5, max / 2 - 5, 10, 10);
+		g.strokeText(ctx.tile.x + ' ' + ctx.tile.y + ' ' + ctx.zoom, max / 2 - 30, max / 2 - 10);
+	},
+
 	_drawPolygon: function (ctx, geom, style) {
+		
+		var el, g, coords, outline, method;
+
 		if (!style) {
 			return;
 		}
 		
-		for (var el = 0; el < geom.length; el++) {
-			var coords = geom[el], proj = [], i;
+		for (el = 0; el < geom.length; el++) {
+			coords = geom[el], proj = [], i;
 			coords = this._clip(ctx, coords);
 			for (i = 0; i < coords.length; i++) {
 				proj.push(this._tilePoint(ctx, coords[i]));
@@ -120,8 +211,8 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 				continue;
 			}
 
-			var g = ctx.canvas.getContext('2d');
-			var outline = style.outline;
+			g = ctx.canvas.getContext('2d');
+			outline = style.outline;
 			g.fillStyle = style.color;
 			if (outline) {
 				g.strokeStyle = outline.color;
@@ -129,7 +220,7 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 			}
 			g.beginPath();
 			for (i = 0; i < proj.length; i++) {
-				var method = (i === 0 ? 'move' : 'line') + 'To';
+				method = (i === 0 ? 'move' : 'line') + 'To';
 				g[method](proj[i].x, proj[i].y);
 			}
 			g.closePath();
@@ -141,17 +232,11 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 	},
 
 	_draw: function (ctx) {
-		// NOTE: this is the only part of the code that depends from external libraries (actually, jQuery only).
-		var loader = $.getJSON;
 
-		var nwPoint = ctx.tile.multiplyBy(this.tileSize);
-		var sePoint = nwPoint.add(new L.Point(this.tileSize, this.tileSize));
-		var nwCoord = this._map.unproject(nwPoint, ctx.zoom, true);
-		var seCoord = this._map.unproject(sePoint, ctx.zoom, true);
-		var bounds = [nwCoord.lng, seCoord.lat, seCoord.lng, nwCoord.lat];
+		var loader = _request, 
+			url = this.getTileUrl(ctx.tile, ctx.zoom),
+			self = this, j;
 
-		var url = this.createUrl(bounds);
-		var self = this, j;
 		loader(url, function (data) {
 			for (var i = 0; i < data.features.length; i++) {
 				var feature = data.features[i];
@@ -195,6 +280,6 @@ L.TileLayer.Canvas.GeoJSON = L.TileLayer.extend({
 						throw new Error('Unmanaged type: ' + type);
 				}
 			}
-		});
+		}, "application/json");
 	}
 });
